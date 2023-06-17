2faf02c03f220a8049d0ca8d1027d3626fa1df5b
martin perez <martin170520@gmail.com>
2023-06-17 09:29:02 -0500

    agregado el service

---
+++ src/main/java/service/ClienteService.java
@@ -0,0 +1,25 @@
+package service;
+
+import com.techdevs.ep3.entity.Cliente;
+import com.techdevs.ep3.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+@Service
+public class ClienteService {
+    private final ClienteRepository clienteRepository;
+
+    public List<Cliente> getAll() {return  clienteRepository.findAll();}
+
+    public Cliente save(Cliente cliente) {return  clienteRepository.save(cliente);}
+
+    public void delete (int id) {
+    clienteRepository.delete(Objects.requireNonNull(clienteRepository.findById(id).orElse(null)));
+    }
+
+
+}
